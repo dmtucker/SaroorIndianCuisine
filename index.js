7f93fe4b755f0ca8b1b70db151d466d9e70e8565
David Tucker <dmtucker@users.noreply.github.com>
2015-05-19 00:45:27 -0700

    Complete the static site.

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-$(document).ready(
-    function ( ) {
-        function resetMenu ( ) {
-            $('nav>ul>li>a').css('color','#FFF');
-        }
-        function activateTab ( that ) {
-            that.find('a').css('color','#06F');
-        }
-        $('nav>ul>li').click(
-            function ( e ) {
-                resetMenu();
-                activateTab($(this));
-            }
-        );
-        $("#tabs").tabs({
-            show: 500
-        });
-        resetMenu();
-        $('nav>ul>li').each(
-            function ( index ) {
-                if ($(this).hasClass('ui-tabs-active')) activateTab($(this));
-            }
-        );
-        $('#map>object').removeClass('absent');
-    }
-);
-
-
+$(function() {
+    $("#tabs").tabs();
+});
 
 
 function slideSwitch() {
@@ -38,3 +13,10 @@ function slideSwitch() {
     });
 }
 $(function() { setInterval("slideSwitch()",3000); });
+
+
+$("#flipbook").turn({
+    width: 1000,
+    height: 670,
+    autoCenter: true
+});
